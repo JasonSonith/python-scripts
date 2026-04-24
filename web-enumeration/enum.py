a9d03aef81878a4b50955b6c50cd6671ae653d39
Jason Sonith <164841606+JasonSonith@users.noreply.github.com>
2026-04-24 13:33:36 -0500

    "Built web enumeration beginning"

---
+++ web-enumeration/enum.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+def run_nmap():
+    pass
+
+def run_gobuster():
+    pass
+
+def run_ffuf():
+    pass
+
+def run_hashcat():
+    pass
+
+def main():
+    subprocess = run_nmap()
+    
+
+if __name__ == '__main__':
+    main()
