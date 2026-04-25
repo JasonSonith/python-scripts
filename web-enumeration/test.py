31c794ca8932105b17581650b6600ec97dfaa175
JasonSonith <JasonSonith1@gmail.com>
2026-04-24 20:26:28 -0500

    Completed the run_nmap function and have a test script to play around with parsing XML

---
+++ web-enumeration/test.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET 
+
+file = 'nmap.xml'
+tree = ET.parse(file)
+root = tree.getroot()
+
+scan = {'ip_address': None}
+
+for elm in root.findall('.//'):
+	print(f"Tag = {elm.tag}")
+	print(f"Attribute={elm.attrib}\n")
+
+# 	if elm.tag == 'address':
+# 		attribute = elm.attrib
+
+ports = {}
+
+for elm in root.findall('.//port'):
+
+	port = int(elm.get('portid'))
+	ports[port] = {}
+	ports[port][service_name] = elm.find('service').get('name')
+
+print(ports)
---
+++ web-enumeration/web-enum.py
@@ -0,0 +1,64 @@
+import subprocess
+import pathlib
+import xml.etree.ElementTree as ET
+import argparse
+import sys
+
+
+parser = argparse.ArgumentParser(description="Automated web enumeration tool")
+parser.add_argument('-ip', type=str, required=True)
+
+try:
+    args = parser.parse_args()
+
+except Exception as e:
+    print(f"[+] Error: No valid IP address passed in.\n Exiting Program...")
+
+ip = args.ip()
+
+class WebEnumeration:
+
+    def __init__(self):
+        self.ip = ip
+
+    def run_nmap(self, ip):
+        subprocess.run('nmap -sC -sV -p- -oX nmap.xml -oN nmap.txt {ip}')
+        file = 'nmap.xml'
+
+        ports = {}
+
+        try:
+            for elm in root.findall('.//port'):
+                port = int(elm.get('portid'))
+                ports[port] = {}
+
+                if elm.find('service') is not None:
+                    ports[port]['service name'] = elm.find('service').get('name')
+                    ports[port]['product'] = elm.find('service').get('product')
+                    ports[port]['version'] = elm.find('service').get('version')
+
+                if elm.find('state') is Not None:
+                    ports[port]['state'] = elm.find('state').get('state')
+
+
+        except Exception as e:
+            print('[+] Error: could not parse XML tree')
+
+            return tags
+
+
+    def run_gobuster(self):
+        pass
+
+    def run_ffuf(self):
+        pass
+
+    def run_hashcat(self):
+        pass
+
+def main():
+    enum = WebEnumeration()
+    enum.run_nmap(ip)
+
+if __name__ == '__main__':
+    main()
